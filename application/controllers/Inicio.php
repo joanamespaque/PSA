37d8b9837cc203155757994b417f602e7829fa19
brendafpacheco <brendapacheco3907@gmail.com>
2019-05-27 09:35:23 -0300

    database connection

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -4,7 +4,7 @@ class Inicio extends CI_Controller{
     public function __construct(){
         parent::__construct();
     }
-    public function inicio (){
+    public function inicio(){
         $this->load->view('header');
         $this->load->view('nav_inicial');
         $this->load->view('pagina_inicial');
