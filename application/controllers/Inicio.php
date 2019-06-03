5a505efa888436c7b8148063152d88fab087a12a
brendafpacheco <brendapacheco3907@gmail.com>
2019-06-03 09:38:27 -0300

    solução para o direcionamento das paginas professor/aluno-> login/cadastro

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -14,4 +14,48 @@ class Inicio extends CI_Controller{
         $this->load->view('nav_inicial');
         $this->load->view('pagina_inicial2');
     }
+
+    public function verifica_acao() {
+        $tipo = $_POST['tipo'];
+        switch($tipo) {
+            case 'cadastro_professor':
+            redirect('cadastro/cadastro_professor');
+                // $this->load->view('header');
+                // $this->load->view('nav_inicial');
+                // $this->load->view('cadastro_professor');
+            break;
+            case 'cadastro_aluno':
+                redirect('cadastro/cadastro_aluno');
+                // $this->load->view('header');
+                // $this->load->view('nav_inicial');
+                // $this->load->view('cadastro');
+            break;
+            case 'login_professor':
+                redirect('login/login_professor');
+                // $this->load->view('header');
+                // $this->load->view('nav_inicial');
+                // $this->load->view('login_professor');
+            break;
+            case 'login_aluno':
+                redirect('login/login_aluno');
+                // $this->load->view('header');
+                // $this->load->view('nav_inicial');
+                // $this->load->view('login_aluno');
+
+            break;
+        }
+        
+        
+        
+        
+        // $this->load->view('header');
+        // $this->load->view($tipo);
+        // $this->load->view('login_aluno');
+
+    }
+
+    // public function sapatos($tipo, $id){
+    //     echo $tipo;
+    //     echo $id;
+    // }
 }
