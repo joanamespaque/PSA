1b740efe26a8a9ee98850445a0e9657b676fdc48
brendafpacheco <brendapacheco3907@gmail.com>
2019-06-03 10:26:06 -0300

    index.php

--- application/config/config.php
+++ application/config/config.php
@@ -34,7 +34,7 @@ $config['base_url'] = 'http://localhost:2000/estagio';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
