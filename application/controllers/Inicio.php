43b0963e9bbab0dc7d2c7c2a3ddd910c9645ecf9
brendafpacheco <brendapacheco3907@gmail.com>
2019-06-10 11:32:41 -0300

    cadastro de aluno e professor

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -20,42 +20,18 @@ class Inicio extends CI_Controller{
         switch($tipo) {
             case 'cadastro_professor':
             redirect('cadastro/cadastro_professor');
-                // $this->load->view('header');
-                // $this->load->view('nav_inicial');
-                // $this->load->view('cadastro_professor');
             break;
             case 'cadastro_aluno':
                 redirect('cadastro/cadastro_aluno');
-                // $this->load->view('header');
-                // $this->load->view('nav_inicial');
-                // $this->load->view('cadastro');
             break;
             case 'login_professor':
                 redirect('login/login_professor');
-                // $this->load->view('header');
-                // $this->load->view('nav_inicial');
-                // $this->load->view('login_professor');
             break;
             case 'login_aluno':
                 redirect('login/login_aluno');
-                // $this->load->view('header');
-                // $this->load->view('nav_inicial');
-                // $this->load->view('login_aluno');
-
             break;
         }
-        
-        
-        
-        
-        // $this->load->view('header');
-        // $this->load->view($tipo);
-        // $this->load->view('login_aluno');
-
+  
     }
 
-    // public function sapatos($tipo, $id){
-    //     echo $tipo;
-    //     echo $id;
-    // }
 }
