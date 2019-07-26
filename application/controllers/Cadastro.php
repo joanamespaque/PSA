a385a278711effe41d186dffdc5864595e4a66b3
liviamenegaro <liviamenegaro@gmail.com>
2019-07-26 02:35:15 -0300

    populando o formulário em caso de erro/ alterações de flashdata

--- application/controllers/Cadastro.php
+++ application/controllers/Cadastro.php
@@ -43,58 +43,77 @@ class Cadastro extends CI_Controller{
     public function enviar() {
         $botao = $_POST['envia_cadastro'];
         if ($botao == 'professor') {
+
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $this->load->model('Professor_model', 'professor_model');
+            $professor = $this->professor_model->buscarUserExistente($email,$username)->result();
+
             $this->form_validation->set_rules('senha', 'Senha', 'required');
             $this->form_validation->set_rules('verificaSenha', 'Verifica Senha', 'matches[senha]|min_length[8]', array('matches' => 'O campo Senha não combina com o campo Verifica Senha', 'min_length' => 'Senha inválida, mínimo de dígitos: 8'));
+            $this->load->helper('email');
+            $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[professor.email]', array('valid_email'=> 'O e-mail inserido é inválido', 'is_unique' => 'O e-mail inserido já existe'));
 
-            if ($this->form_validation->run() == FALSE) {
-            // if ($this->form_validation->run('register_prof') == FALSE) {
+            if ($professor) {
+                $this->form_validation->set_rules('username', 'Nome de usuário', 'is_unique[professor.username]', array('is_unique'=>'O nome de usuário inserido já existe'));
+            }
+
+            if ($this->form_validation->run() == FALSE ) {
                 $this->session->set_flashdata('error', validation_errors());
-                redirect('cadastro/cadastro_professor');
+                $this->load->view('header');
+                $this->load->view('nav_inicial');
+                $this->load->view('cadastro_professor');
 
             } else {
-                $this->load->model('Professor_model', 'professor_model');
                 // Validação do formulário correta!
                 $dados['email'] = $_POST['email'];
                 $dados['username'] = $_POST['username'];
                 $dados['senha'] = $_POST['senha'];
                 $dados['nome'] = $_POST['primeiroNome'];
                 $dados['sobrenome'] = $_POST['sobrenome'];
-                $this->load->model('Professor_model', 'professor_model');
-                if (empty($this->professor_model->buscarUserExistente($dados['email'], $dados['username'])->result())) {
-                    $inserir = $this->professor_model->cadastrar($dados);
-                    if ($inserir == True) {
-//                
-                        $usuario = $this->professor_model->buscarUsuario($dados)->result();
 
-                        $newdata = array(
-                            'nome'  => $usuario[0]->nome,
-                            'sobrenome' => $usuario[0]->sobrenome,
-                            'username' => $usuario[0]->username,
-                            'email' => $usuario[0]->email,
-                            'tipo_user' => 'professor',
-                            'logged_in' => TRUE
-                        );
-                        $this->session->set_userdata($newdata);
-                        redirect('turmas/turmas_professor');
+                $inserir = $this->professor_model->cadastrar($dados);
+                if ($inserir == True) {
+//                
+                    $usuario = $this->professor_model->buscarUsuario($dados)->result();
+
+                    $newdata = array(
+                        'nome'  => $usuario[0]->nome,
+                        'sobrenome' => $usuario[0]->sobrenome,
+                        'username' => $usuario[0]->username,
+                        'email' => $usuario[0]->email,
+                        'tipo_user' => 'professor',
+                        'logged_in' => TRUE
+                    );
+                    $this->session->set_userdata($newdata);
+                    redirect('turmas/turmas_professor');
 // 
-                    } else {
-                        $this->session->set_flashdata('error', 'Cadastro não pôde ser realizado.');
-                        redirect('cadastro/cadastro_professor');
-                    }
                 } else {
-                    $this->session->set_flashdata('error', 'Cadastro não pôde ser realizado: nome de usuário e/ou email já existe(m).');
+                    $this->session->set_flashdata('error', 'Cadastro não pôde ser realizado por problema no servidor.');
                     redirect('cadastro/cadastro_professor');
                 }
             }   
-            //AQUI COMEÇA O ALUNO
+
+
+        //AQUI COMEÇA A VERIFICAÇÃO DE ALUNO
         } else if ($botao == 'aluno') {
+
+            $username = $_POST['username'];
+            $this->load->model('Aluno_model', 'aluno_model');
+            $aluno = $this->aluno_model->buscarUserExistente($username)->result();
+
             $this->form_validation->set_rules('senha', 'Senha', 'required');
             $this->form_validation->set_rules('verificaSenha', 'Verifica Senha', 'matches[senha]|min_length[8]', array('matches' => 'O campo Senha não combina com o campo Verifica Senha', 'min_length' => 'Senha inválida, mínimo de dígitos: 8'));
-
+            if ($aluno) {
+                $this->form_validation->set_rules('username', 'Nome de usuário', 'is_unique[aluno.username]', array('is_unique'=>'O nome de usuário inserido já existe'));
+            }
+            
             if ($this->form_validation->run() == FALSE) {
-            // if ($this->form_validation->run('register_prof') == FALSE) {
+
                 $this->session->set_flashdata('error', validation_errors());
-                redirect('cadastro/cadastro_aluno');
+                $this->load->view('header');
+                $this->load->view('nav_inicial');
+                $this->load->view('cadastro_aluno');
 
             } else {
                 // Validação do formulário correta!
@@ -102,37 +121,30 @@ class Cadastro extends CI_Controller{
                 $dados['sobrenome'] = $_POST['sobrenome'];
                 $dados['senha'] = $_POST['senha'];
                 $dados['username'] = $_POST['username'];
-                $this->load->model('Aluno_model', 'aluno_model');
-                // $inserir = $this->aluno_model->cadastrar($dados);
 
-                if(empty($this->aluno_model->buscarUserExistente($dados['username'])->result())){
-                    $inserir = $this->aluno_model->cadastrar($dados);
-                    if ($inserir == True) {
+                $inserir = $this->aluno_model->cadastrar($dados);
+                if ($inserir == True) {
                         //                
-                        $usuario = $this->aluno_model->buscarUsuario($dados)->result();
+                    $usuario = $this->aluno_model->buscarUsuario($dados)->result();
 
-                        $newdata = array(
-                            'nome'  => $usuario[0]->nome,
-                            'sobrenome' => $usuario[0]->sobrenome,
-                            'username' => $usuario[0]->username,
-                            'tipo_user' => 'aluno',
-                            'logged_in' => TRUE
-                        );
-                        $this->session->set_userdata($newdata);
-                        redirect('turmas/turmas_aluno');
+                    $newdata = array(
+                        'nome'  => $usuario[0]->nome,
+                        'sobrenome' => $usuario[0]->sobrenome,
+                        'username' => $usuario[0]->username,
+                        'tipo_user' => 'aluno',
+                        'logged_in' => TRUE
+                    );
+                    $this->session->set_userdata($newdata);
+                    redirect('turmas/turmas_aluno');
 //
-                    } else {
-                        $this->session->set_flashdata('error', 'Cadastro não pôde ser realizado.');
-                        redirect('cadastro/cadastro_aluno');
-                    }
                 } else {
-                    $this->session->set_flashdata('error', 'Cadastro não pôde ser realizado: nome de usuário já existe.');
+                    $this->session->set_flashdata('error', 'Cadastro não pôde ser realizado por problema no servidor.');
                     redirect('cadastro/cadastro_aluno');
                 }
-          }
+        }
 
         }
-     
+
     }
 }
 
--- application/views/cadastro_aluno.php
+++ application/views/cadastro_aluno.php
@@ -7,12 +7,13 @@
     <link rel="stylesheet" href="<?= base_url();?>/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= base_url();?>/assets/css/header.css" media="all">
     <link rel="stylesheet" href="<?= base_url();?>/assets/css/cadastro.css">
-    <title>Cadastro Professor</title>
+    <title>Cadastro Aluno</title>
 </head>
 <body>
         <!-- <div class="caminho"></div> espaço para breadcrumbs -->
         <h2 class="acessibilidadeFonte">Cadastre-se</h2>
         <div class="formulario row">
+            
             <form action="<?= base_url()?>cadastro/enviar" method="POST"> <!-- Aqui falta o caminho -->
             <?php if($this->session->flashdata('error')) {?>
 					<div class="alert alert-danger text-center" style="margin-top:20px;">
@@ -24,27 +25,27 @@
                     <li>
                         <label for="nomeDeUsuario" class="acessibilidadeFonte">Nome de Usuário</label>
                         <br>
-                        <input class="form-control" type="text" name="username" maxlength="150" required>
+                        <input class="form-control" type="text" name="username" maxlength="150" value="<?= set_value('username');?>" required>
                     </li>
                     <li>
                         <label for="senha" class="acessibilidadeFonte">Senha</label>
                         <br>
-                        <input class="form-control" type="password" name="senha" maxlength="20" required>
+                        <input class="form-control" type="password" name="senha" maxlength="20" value="<?= set_value('senha');?>"required>
                     </li>
                     <li>
                         <label for="verificaSenha" class="acessibilidadeFonte">Verificar Senha</label>
                         <br>
-                        <input class="form-control" type="password" name="verificaSenha" maxlength="20" required>
+                        <input class="form-control" type="password" name="verificaSenha" maxlength="20" value="<?= set_value('verificaSenha');?>" required>
                     </li>
                     <li>
                         <label for="primeiroNome" class="acessibilidadeFonte">Primeiro Nome</label>
                         <br>
-                        <input class="form-control" type="text" name="primeiroNome" maxlength="150" required>
+                        <input class="form-control" type="text" name="primeiroNome" maxlength="150" value="<?= set_value('primeiroNome');?>" required>
                     </li>
                     <li>
                         <label for="sobrenome" class="acessibilidadeFonte">Sobrenome</label>
                         <br>
-                        <input class="form-control" type="text" name="sobrenome" maxlength="150" required>
+                        <input class="form-control" type="text" name="sobrenome" maxlength="150" value="<?= set_value('sobrenome');?>" required>
                     </li>
                 <button class="acessibilidadeFonte button-cadastro cadastroUsuario" id="cadastrarAluno" type="submit" name="envia_cadastro" value="aluno">CADASTRAR</button>
             </form>
