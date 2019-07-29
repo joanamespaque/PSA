ac2118ecc9625ad13c0586b703df48f05985ad6c
liviamenegaro <liviamenegaro@gmail.com>
2019-07-29 11:35:03 -0300

    model parcial de turmas

---
+++ application/models/Turmas_model.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Turmas_model extends CI_Model { 
+    
+    // LISTAR TODAS AS TURMAS
+    public function listar() {
+        $this->db->select('nome');
+        $this->db->from('turma');
+        $result = $this->db->get();
+    }
+    // LISTAR OS ALUNOS DA TURMA
+    public function listarAlunoTurma() {
+        $this->db->select('*');
+        $this->db->from('turma');
+        $this->db->join('alunoturma', 'turma.id = alunoturma.idturma');
+        $this->db->join('aluno', 'alunoturma.idaluno = aluno.id');
+        // $this->db->where('turma.id', $valor);        BUSCA COM ID 
+        $result = $this->db->get();
+
+        return $result;
+    }
+    // PROFESSOR CADASTRA UMA TURMA 
+    public function cadastroTurma($dados=NULL) {
+        if ($dados!=NULL) {
+            $this->db->set($dados);
+            $this->db->insert('turma');
+
+            return True;
+        } else {
+            return False;
+        }
+    }
+    // PROFESSOR ADICIONA ALUNOS À TURMA 
+    public function cadastroAlunoTurma($dados=NULL) {
+        if ($dados!=NULL) {
+            $this->db->set($dados);
+            $this->db->insert('alunoturma');
+            
+            return True;
+        } else {
+            return False;
+        }
+    }
+    
+}
